78f6498d158eaf232e8237b3450a3b491a2e74e0
Claiton Nazaret <claitonnazaret@gmail.com>
2025-02-21 23:15:42 -0300

    chore: add migrations prisma and update initial data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,13 +7,20 @@ const userData: Prisma.UserCreateInput[] = [
     name: 'Administrador Sistema',
     email: 'claitonnazaret@gmail.com',
     image: 'https://github.com/claitonnazaret.png',
-    password: '$2a$12$uOlV7veyhWTnFJ0LLoY/Q.Z6rf9PcwFYALE83hw6UXHEX8hsSa8fy', //admin123
+    password: '$2a$12$uOlV7veyhWTnFJ0LLoY/Q.Z6rf9PcwFYALE83hw6UXHEX8hsSa8fy',
     userRoles: {
       create: [
         { role: { create: { name: 'admin' } } },
         { role: { create: { name: 'client' } } },
       ],
-    },
+    }, // admin123
+  },
+  {
+    name: 'Cliente Sistema',
+    email: 'client123@gmail.com',
+    image: 'https://jsonplaceholder.typicode.com/mockend.svg',
+    password: '$2a$12$uL3Mpl//FH.AzzReLhZISu3.90aR4ruaqVaX6dYdW8Ho8dwHZnrE6',
+    userRoles: { create: { role: { connect: { name: 'client' } } } }, // client123
   },
 ]
 
